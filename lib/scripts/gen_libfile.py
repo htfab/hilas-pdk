01998649c9676bd084f6b8e60e7f17e256860a7f
Barry Muldrey <bjmuld@users.noreply.github.com>
2020-12-05 00:57:58 -0600

    templating lib file generation

--- lib/scripts/gen_libfile.py
+++ lib/scripts/gen_libfile.py
@@ -5,13 +5,14 @@ import re
 import os
 import argparse
 
+import jinja2
 from pathlib import Path
 
 this_dir = Path(__file__).parent
 STD_CELLS_PATH = this_dir / '..' / '..' / 'magic'
 # TARGET_LIB_PATH = this_dir/'..'/'hilas.lib'
-REF_CELL_PATH = this_dir/'min_inverter_template.lib.cell'
-LIB_TEMPLATE = this_dir/'template_nocells.lib'
+CELL_TEMPLATE = 'min_inverter.template.lib'
+LIB_TEMPLATE = 'head.template.lib'
 
 
 def main():
@@ -21,18 +22,25 @@ def main():
 
     cell_names = [x.strip('.mag') for x in os.listdir(STD_CELLS_PATH) if x.endswith('.mag')]
 
-    with open(REF_CELL_PATH, 'r') as f:
-        ref_cell = f.read()
+    templateLoader = jinja2.FileSystemLoader('.')
+    templateEnv = jinja2.Environment(
+        loader=templateLoader,
+        autoescape=jinja2.select_autoescape(
+            disabled_extensions=('lib',),
+            default_for_string=True,
+            default=True,
+        ))
 
-    with open(LIB_TEMPLATE, 'r') as f:
-        template = f.read()
+    lib_template = templateEnv.get_template(str(LIB_TEMPLATE))
+    cell_template = templateEnv.get_template(str(CELL_TEMPLATE))
 
     cell_block = []
     for cn in cell_names:
-        cell_block.append(re.sub('\{\{cell_name\}\}', cn, ref_cell))
+        cell_block.append(cell_template.render(cell_name=cn))
 
+    result = lib_template.render(cells='\n\n'.join(cell_block))
     with open(args.output, 'w') as f:
-        f.write(re.sub(r'\{\{cells\}\}', '\n\n'.join(cell_block), template))
+        f.write(result)
 
 
 if __name__ == '__main__':
